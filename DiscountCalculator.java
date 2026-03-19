92519293a237774008cc7b49d85aaffa34a461e0
Arthur e Uzires <arthur1515felipe@gmail.com>
2026-03-18 21:41:34 -0300

    codigo certo

--- DiscountCalculator.java
+++
@@ -1,7 +0,0 @@
-public class DiscountCalculator {
-    private static final double DISCOUNT_RATE = 0.1;
-
-    public static double applyDiscount(double total) {
-        return total - (total * DISCOUNT_RATE);
-    }
-}
--- ReportBuilder.java
+++
@@ -1,7 +0,0 @@
-public class ReportBuilder {
-    public static String buildReport(String reportName, double total, String status) {
-        return "Relatorio: " + reportName + "\n" +
-               "Total: " + total + "\n" +
-               "STATUS: " + status;
-    }
-}
